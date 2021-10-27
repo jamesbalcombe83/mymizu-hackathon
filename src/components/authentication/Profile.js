88ea51e2f9ba547a711722268c6e43cd6ede7d5e
SumitVermakgp <sumitverma9628@gmail.com>
2021-10-27 12:01:38 +0900

    wip

--- src/components/authentication/Profile.js
+++ src/components/authentication/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const axios = require('axios');
@@ -8,28 +8,42 @@ const Profile = () => {
   const [userDetails, setUserDetails] = useState();
   const { user, isAuthenticated, isLoading } = useAuth0();
 
-  if (isLoading) {
-    return <div>Loading ...</div>;
+  if (isLoading && !isAuthenticated ) {
+    return null;
+  } else {
+    useEffect (() => { 
+      try {
+        const get_business_user_url = "/business_user?id=" + String(user.sub);
+        console.log(get_business_user_url)
+        axios.get(get_business_user_url)
+        .then((response) => setUserDetails(response.data));
+      } catch (error) {
+        console.log(error);
+      }
+      console.log(userDetails)
+    }, []);
   }
 
- 
-  try {
-    let get_business_user_url = "/business_user/:" + String(user.sub);
-    axios.get(get_business_user_url)
-    .then((response) => setUserDetails(response.data));
-  } catch (error) {
-    console.log(error);
-  }
+  //if (isLoading) {
+  //  return <div>Loading ...</div>;
+//}
+
+  
+
+  
 
 
 // <h2>{user.sub}</h2>
 // <p>{user.email}</p>
+// <p> {userDetails.business_user} </p>
+//<p> {userDetails.tap} </p>
+// <p> {userDetails.business_user[0].business_name}</p>
 
   return (
     isAuthenticated && (
       <div>
-        <p> {userDetails.business_user} </p>
-        <p> {userDetails.tap} </p>
+        <p> {user.email} </p>
+        
       </div>
     )
   );
