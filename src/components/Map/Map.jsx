fdc36c7ab9abbb406d98dc2f0cdff178a42e7998
SumitVermakgp <sumitverma9628@gmail.com>
2021-10-28 01:45:58 +0900

    revert changes in map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Map, Marker, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import { Component } from 'react'
 
 const apiKey = process.env.REACT_APP_API_KEY;
