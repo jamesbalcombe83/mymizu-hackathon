3887d0907439cdbe4eb33f4a24476bc9db266c0c
jamesbalcombe83 <james.balcombe83@gmail.com>
2021-10-27 09:47:35 +0900

    Feat: added mymizu db access

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const knex = require('knex');
+const mymizudb = require('./mymizudb');
 
 const db = knex({
     client: 'pg',
@@ -22,6 +23,21 @@ app.use(express.static(path.resolve(__dirname, '../build')));
 
 app.get('/api', (req, res) => {
     res.send('This is a response from the server');
+})
+
+//test route to check connection to the mymizu database
+app.get('/test', async (req,res) => {
+    try {
+        const taps = await mymizudb
+          .select('id')
+          .table("taps")
+          .where({category_id : 4})
+          .whereNotNull('name')
+        res.send(taps);
+      } catch (err) {
+        console.error("Error loading refill!", err);
+        res.sendStatus(500);
+      }
 })
 
 app.get('*', (request, response) => {
