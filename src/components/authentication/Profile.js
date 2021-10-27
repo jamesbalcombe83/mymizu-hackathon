c8e2d9eef40d62e0303537c26ad5ea73f15e8136
SumitVermakgp <sumitverma9628@gmail.com>
2021-10-27 11:08:20 +0900

    show user details

--- src/components/authentication/Profile.js
+++ src/components/authentication/Profile.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
+const axios = require('axios');
+
 const Profile = () => {
+
+  const [userDetails, setUserDetails] = useState();
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
   }
 
+ 
+  try {
+    let get_business_user_url = "/business_user/:" + String(user.sub);
+    axios.get(get_business_user_url)
+    .then((response) => setUserDetails(response.data));
+  } catch (error) {
+    console.log(error);
+  }
+
+
+// <h2>{user.sub}</h2>
+// <p>{user.email}</p>
+
   return (
     isAuthenticated && (
       <div>
-        <h2>{user.sub}</h2>
-        <p>{user.email}</p>
+        <p> {userDetails.business_user} </p>
+        <p> {userDetails.tap} </p>
       </div>
     )
   );
